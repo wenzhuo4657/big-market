1b72d9ca3a94298e8fb5903a397f10ff57c76704
wenzhuo4657 <14783149521@163.com>
2024-11-22 19:20:43 +0800

    no message

--- big-market-trigger/src/main/java/cn/wenzhuo4657/BigMarket/trigger/http/RaffleActivityController.java
+++ big-market-trigger/src/main/java/cn/wenzhuo4657/BigMarket/trigger/http/RaffleActivityController.java
@@ -108,6 +108,7 @@ public class RaffleActivityController implements IRaffleActivityService {
                     .build();
         }
     }
+      //  wenzhuo TODO 2024/11/19 :  查询得知hystrix基于线程池或者信号量实现资源隔离，待优化，或者更换组件，
 
     @Override
     @HystrixCommand(commandProperties = {

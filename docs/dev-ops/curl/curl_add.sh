7c398a5cc9c44225d2f220a2d7c03171257054bf
wenzhuo4657 <14783149521@163.com>
2025-05-23 16:20:24 +0800

    init

--- docs/dev-ops/curl/curl_add.sh
+++
@@ -1,8 +0,0 @@
-curl -X PUT "117.72.36.124:9200/xfg_dev_tech.user_order/_mapping" -H 'Content-Type: application/json' -d'
-{
-  "properties": {
-    "_sku_name": {
-      "type": "text"
-    }
-  }
-}'
